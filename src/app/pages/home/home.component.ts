b972b15d355bd08aa1e117889abae2efcc0ce867
omarsmarinhos <omarmarinosaguilar@gmail.com>
2024-02-21 23:47:37 -0500

    Forms control

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { Task } from '../../models/task.model';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Crear Proyecto',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Crear componentes',
+      completed: false
+    },
+  ]);
 
+  newTaskCtrl = new FormControl('', {
+    nonNullable: true,
+    validators: [
+      Validators.required
+    ]
+  });
+
+  changeHandler() {
+    if (this.newTaskCtrl.valid) {
+      const value = this.newTaskCtrl.value.trim();
+      if (value !== '') {
+        this.addTask(value);
+        this.newTaskCtrl.setValue('');
+      }
+    }
+  }
+
+  addTask(title: string) {
+    const newTask: Task = {
+      id: Date.now(),
+      title: title,
+      completed: false
+    }
+    this.tasks.update((tasks) => [...tasks, newTask]);
+
+  }
+
+  deleteTask(i: number) {
+    this.tasks.update((tasks) => tasks.filter((task, position) => i !== position));
+  }
+
+  updateTask(i: number) {
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if (position === i) {
+          return {
+            ...task,
+            completed: !task.completed
+          }
+        }
+        return task;
+      });
+    });
+  }
 }
--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,22 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
 export class LabsComponent {
   welcome = 'Hola';
-  tasks = [
+  tasks = signal([
     'Instalar el Angular CLI',
     'Crear el proyecto',
     'Crear componentes',
     'Crear servicio'
-  ]
-  name= 'Omar';
+  ]);
+
+  name = signal('Omar');
   disabled = true;
   img = 'http://w3schools.com/howto/img_avatar.png';
 
@@ -26,12 +28,21 @@ export class LabsComponent {
     avatar: 'http://w3schools.com/howto/img_avatar.png'
   }
 
+  colorCtrl = new FormControl();
+
+  constructor() {
+    this.colorCtrl.valueChanges.subscribe(value => {
+      console.log(value);
+    })
+  }
+
   clickHandler() {
     alert('Hola');
   }
 
   changeHandler(e: Event) {
-    console.log(e);
+    const input = e.target as HTMLInputElement;
+    this.name.set(input.value);
   }
 
   keydownHandler(e: KeyboardEvent) {
